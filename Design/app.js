034154457a5954cad6ffb2f7b00e90ef30e1da27
GRoszhart <136271882+GRoszhart@users.noreply.github.com>
2023-12-03 18:36:17 -0600

    Add files via upload

--- Design/app.js
+++ Design/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const db = require('./database/db');
 const authenticationRoutes = require('./routes/authentication');
 const recipesRoutes = require('./routes/recipes');
+const inventoryRoutes = require('./routes/inventory');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// EJS
+// EJS viewset
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 // Database connection middleware
 app.use((req, res, next) => {
@@ -22,11 +25,13 @@ app.use((req, res, next) => {
 
 // Routes
 const indexRoutes = require('./routes/index');
-app.use('/', indexRoutes);
+app.use('/index', indexRoutes);
 
 app.use('/authentication', authenticationRoutes);
 
 app.use('/recipes', recipesRoutes);
+
+app.use('/inventory', inventoryRoutes);
 
 // Start the server
 app.listen(PORT, () => {
